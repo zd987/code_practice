15d68fa567031678ff6d8f7569265617565b1807
Dong Zhao <zhaodong8701@gmail.com>
2013-12-01 23:52:00 +0800

    fix an error

--- workspace/LeetCode/dfs/solveSudoku/Solution.java
+++ workspace/LeetCode/dfs/solveSudoku/Solution.java
@@ -19,51 +19,49 @@ import java.util.Arrays;
  * Version: 1.0
  */
 public class Solution {
-	public class Solution {
-	    class Pair{
-	        int x, y, val;
-	        public Pair(int x, int y){
-	            this.x = x;
-	            this.y = y;
-	            this.val = 0;
-	        }
-	    }
-	    public void solveSudoku(char[][] board) {
-	        ArrayList<Pair> list = new ArrayList<Pair>();
-	        int i, j, k;
-	        for(i = 0; i < 9; ++i){
-	            for(j = 0; j < 9; ++j){
-	                if(board[i][j] == '.') list.add(new Pair(i, j));
-	            }
-	        }
-	        k = 0;
-	        boolean[] b = new boolean[10];
-	        while(k >= 0 && k < list.size()){
-	            Pair p = list.get(k);
-	            board[p.x][p.y] = '.';
-	            Arrays.fill(b, false);
-	            for(i = 0; i < 9; ++i){
-	                if(board[i][p.y] != '.') b[board[i][p.y] - '0'] = true;
-	                if(board[p.x][i] != '.') b[board[p.x][i] - '0'] = true;
-	            }
-	            int x = p.x / 3, y = p.y / 3;
-	            for(i = 0; i < 3; ++i){
-	                for(j = 0; j < 3; ++j){
-	                    if(board[3 * x + i][3 * y + j] != '.') b[board[3 * x + i][3 * y + j] - '0'] = true;
-	                }
-	            }
-	            for(i = p.val + 1; i <= 9 && b[i]; ++i) ;
-	            if(i <= 9) {
-	                board[p.x][p.y] = (char) ('0' + i);
-	                p.val = i;
-	                ++k;
-	            } else {
-	                p.val = 0;
-	                --k;
-	            }
-	        }
-	    }
-	}
+	class Pair{
+        int x, y, val;
+        public Pair(int x, int y){
+            this.x = x;
+            this.y = y;
+            this.val = 0;
+        }
+    }
+    public void solveSudoku(char[][] board) {
+        ArrayList<Pair> list = new ArrayList<Pair>();
+        int i, j, k;
+        for(i = 0; i < 9; ++i){
+            for(j = 0; j < 9; ++j){
+                if(board[i][j] == '.') list.add(new Pair(i, j));
+            }
+        }
+        k = 0;
+        boolean[] b = new boolean[10];
+        while(k >= 0 && k < list.size()){
+            Pair p = list.get(k);
+            board[p.x][p.y] = '.';
+            Arrays.fill(b, false);
+            for(i = 0; i < 9; ++i){
+                if(board[i][p.y] != '.') b[board[i][p.y] - '0'] = true;
+                if(board[p.x][i] != '.') b[board[p.x][i] - '0'] = true;
+            }
+            int x = p.x / 3, y = p.y / 3;
+            for(i = 0; i < 3; ++i){
+                for(j = 0; j < 3; ++j){
+                    if(board[3 * x + i][3 * y + j] != '.') b[board[3 * x + i][3 * y + j] - '0'] = true;
+                }
+            }
+            for(i = p.val + 1; i <= 9 && b[i]; ++i) ;
+            if(i <= 9) {
+                board[p.x][p.y] = (char) ('0' + i);
+                p.val = i;
+                ++k;
+            } else {
+                p.val = 0;
+                --k;
+            }
+        }
+    }
     private void print(char[][] chs){
     	for(int i = 0; i < 9; ++i){
     		System.out.print(new String(chs[i])+ ", ");
