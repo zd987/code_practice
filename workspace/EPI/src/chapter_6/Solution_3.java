b165fddba96d4dd3668249f50ba3d31e64998c10
Dong Zhao <zhaodong8701@gmail.com>
2013-12-09 20:47:08 +0800

    minor update

--- workspace/EPI/src/chapter_6/Solution_3.java
+++ workspace/EPI/src/chapter_6/Solution_3.java
@@ -16,5 +16,14 @@ package chapter_6;
  * Version: 1.0
  */
 public class Solution_3 {
-
+	int minBattery(int[] A){
+		int n = A.length, i, j, k, min = A[0], re = 0;
+		for(i = 1; i < n; ++i){
+			if(A[i] - min > re){
+				re = A[i] - min;
+			}
+			min = Math.min(min, A[i]);
+		}
+		return re;
+	}
 }
