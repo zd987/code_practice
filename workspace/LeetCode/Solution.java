956a8c517cb659cf7289ff5e731505c0d9b13634
Dong Zhao <zhaodong8701@gmail.com>
2013-12-21 13:18:24 +0800

    update

--- workspace/LeetCode/Solution.java
+++ workspace/LeetCode/Solution.java
@@ -6,74 +6,51 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Solution {
-    private void r(String start, ArrayList<ArrayList<String>> re, ArrayList<String> cur, 
-        HashMap<String, ArrayList<String>> m, String word){
-            if(word.equals(start)){
-            	ArrayList<String> tmp = new ArrayList<String>(cur);
-                Collections.reverse(tmp);
-                re.add(tmp);
-                return;
+    public String minWindow(String S, String T) {
+        HashMap<Character, Integer> m = new HashMap<Character, Integer>();
+        int i, j, k, ns = S.length(), nt = T.length(), len = -1, re_start = -1, start = 0, cnt = 0;
+        for(i = 0; i < nt; ++i){
+            Integer t = m.get(T.charAt(i));
+            if(t == null){
+                t = 0;
             }
-            ArrayList<String> list = m.get(word);
-            for(String s : list){
-            	cur.add(s);
-            	r(start, re, cur, m, s);
-            	cur.remove(cur.size() - 1);
-            }
+            m.put(T.charAt(i), t + 1);
         }
-    public ArrayList<ArrayList<String>> findLadders(String start, String end, HashSet<String> dict) {
-        HashMap<String, ArrayList<String>> m = new HashMap<String, ArrayList<String>>();
-        Queue<String> q = new LinkedList<String>();
-        q.offer(start);
-        boolean find = false;
-        int i, j, k;
-        HashSet<String> visited = new HashSet<String>();
-        while(!q.isEmpty() && !find){
-            int qsize = q.size();
-            HashSet<String> cur = new HashSet<String>();
-            for(k = 0; k < qsize; ++k){
-                String str = q.poll();
-                for(i = 0; i < str.length(); ++i){
-                    char ch = str.charAt(i);
-                    for(char c = 'a'; c <= 'z'; ++c){
-                        if(c != ch){
-                            StringBuilder sb = new StringBuilder(str);
-                            sb.setCharAt(i, c);
-                            String s = sb.toString();
-                            if(s.equals(end)) find = true;
-                            if((s.equals(end) || dict.contains(s)) && !visited.contains(s)){
-                                ArrayList<String> list = m.get(s);
-                                if(list == null){
-                                    list = new ArrayList<String>();
-                                    m.put(s, list);
+        for(i = 0; i < ns; ++i){
+            if(m.containsKey(S.charAt(i))){
+                Integer t = m.get(S.charAt(i));
+                m.put(S.charAt(i), t - 1);
+                if(t > 0){
+                    cnt++;
+                    if(cnt == nt){
+                        for(j = start; j <= i; ++j){
+                            Integer tmp = m.get(S.charAt(j));
+                            if(tmp != null){
+                                ++tmp;
+                                m.put(S.charAt(j), tmp);
+                                if(tmp > 0){
+                                    break;
                                 }
-                                list.add(str);
-                                if(!cur.contains(s)){
-                                    q.offer(s);
-                                }
-                                cur.add(s);
                             }
                         }
+                        if(len == -1 || i - j + 1 < len){
+                            len = i - j + 1;
+                            re_start = j;
+                        }
+                        cnt--;
+                        start = j + 1;
                     }
                 }
             }
-            visited.addAll(cur);
         }
-        ArrayList<ArrayList<String>> re = new ArrayList<ArrayList<String>>();
-        if(find){
-	        ArrayList<String> cur = new ArrayList<String>();
-	        cur.add(end);
-	        r(start, re, cur, m, end);
+        if(re_start != -1){
+            return S.substring(re_start, re_start + len);
+        } else {
+            return "";
         }
-        return re;
     }
     public static void main(String[] args) {
-		String start = "hot";
-		String end = "dog";
-		String[] ar = {"hot","dog"};
-		HashSet<String> dict = new HashSet<String>();
-		for(String s : ar) dict.add(s);
 		Solution sol = new Solution();
-		sol.findLadders(start, end, dict);
+		System.out.println(sol.minWindow("ADOBECODEBANC", "ABC"));
 	}
 }
